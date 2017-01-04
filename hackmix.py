a65cb9d4ccd9acf4230a7a342f80ed0e78637c4b
allane <allane.lalmi@native-instruments.de>
2017-01-04 16:30:30 +0100

    Trying to make the stuff work yeh

--- hackmix.py
+++ hackmix.py
@@ -1,30 +1,5 @@
+from tracks import db
 
-# This is the "database" the program looks into, format is: "Artist name - Track title" [BPM, root key #]
-db = {
-    "Proxima - Trapped" : [140, 4],
-    "Commodo - Sleepwave" : [137, 6],
-    "Roska - Spearhead" : [140, 3],
-    "Loefah - Disko Rekah": [145, 8],
-    "Lurka - Full Clip": [90, 4],
-    "James Blake - Sparing The Horses": [140, 1],
-    "Burial - Unite": [138, 2],
-    "Joy Orbison - Hyph Mngo": [140, 2],
-    "Dub War - The Funky Deal": [140, 12],
-    "Skream - Bahl Fwd": [140, 5],
-    "Four Tet - Spirit Fingers": [83, 12],
-    "Las - Tic": [140, 1],
-    "Mala - Cuba Electronic": [140, 3],
-    "Boddika - When I Dip": [137,4],
-    "Pinch - The Boxer": [142,5],
-    "2562 - Techno Dread": [140, 7],
-    "Egoless - I'm From The Balkans": [140, 9],
-    "Karma - Cha": [140, 10],
-    "Gantz - Spry Sinister": [140,8],
-    "Kryptic Minds - Six Degrees": [140, 6],
-    "Pinch - Dr Carlson": [142,11],
-    "Loefah - Goat Stare": [136, 4]
-}
-# TODO: LEARN MYSQL OR SOMETHING
 
 # keys_dict = {'A': 8, 'A#': 3, 'B': 10,'C': 5,'C#': 12,'D': 7,'D#': 2,'E': 9,'F': 4,'F#': 11,'G': 6,'G#': 1}
 # TODO
@@ -69,12 +44,12 @@ def hackmix():
     
 # Checks in database for BPM matches
     def bpm_match(bpm):
-        for i in db:
+        for i, v in db.items():
             i_bpm = db[i][0]
             if bpm in range (i_bpm - 3, i_bpm + 3):
                 results_bpm.append(i)
             if bpm in range (int(i_bpm * 1.04), int(i_bpm * 1.06)):
-                results_bpm_2[db[i]] = db[i][0,1]
+                results_bpm_2[i] = v
                 
 
 
