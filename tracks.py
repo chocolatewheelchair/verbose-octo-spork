78bb9ddc3b11f8047419c52c770412b8c6798a6b
allane <allane.lalmi@native-instruments.de>
2017-01-04 16:28:37 +0100

    Database in another module

---
+++ tracks.py
@@ -0,0 +1,28 @@
+
+
+# This is the "database" the program looks into, format is: "Artist name - Track title" [BPM, root key #]
+db = {
+    "Proxima - Trapped" : [140, 4],
+    "Commodo - Sleepwave" : [137, 6],
+    "Roska - Spearhead" : [140, 3],
+    "Loefah - Disko Rekah": [145, 8],
+    "Lurka - Full Clip": [90, 4],
+    "James Blake - Sparing The Horses": [140, 1],
+    "Burial - Unite": [138, 2],
+    "Joy Orbison - Hyph Mngo": [140, 2],
+    "Dub War - The Funky Deal": [140, 12],
+    "Skream - Bahl Fwd": [140, 5],
+    "Four Tet - Spirit Fingers": [83, 12],
+    "Las - Tic": [140, 1],
+    "Mala - Cuba Electronic": [140, 3],
+    "Boddika - When I Dip": [137,4],
+    "Pinch - The Boxer": [142,5],
+    "2562 - Techno Dread": [140, 7],
+    "Egoless - I'm From The Balkans": [140, 9],
+    "Karma - Cha": [140, 10],
+    "Gantz - Spry Sinister": [140,8],
+    "Kryptic Minds - Six Degrees": [140, 6],
+    "Pinch - Dr Carlson": [142,11],
+    "Loefah - Goat Stare": [136, 4]
+}
+# TODO: LEARN MYSQL OR SOMETHING
