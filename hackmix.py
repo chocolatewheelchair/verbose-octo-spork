52654e930cfff39b74c1dc231a3aa9595e107cd0
allane <allane.lalmi@native-instruments.de>
2017-01-05 17:58:26 +0100

    More dilettante mathematics

--- hackmix.py
+++ hackmix.py
@@ -2,7 +2,7 @@ from tracks import db
 
 
 
-# TODO
+# TODO or maybe not
 # Converts the user input key into a number in range(1,12) for convenience
 # def key_convert(key):
 #     keys_dict = {'A': 8, 'A#': 3, 'B': 10,'C': 5,'C#': 12,'D': 7,'D#': 2,'E': 9,'F': 4,'F#': 11,'G': 6,'G#': 1}
@@ -60,12 +60,21 @@ def hackmix():
                 results_bpm_minus2[i] = v
             if bpm in range (int(i_bpm * 0.939), int(i_bpm * 0.92)):
                 results_bpm_minus4[i] = v
-                
+# The following part changes the root keys to reflect the pitch change on a turntable               
         for i, v in results_bpm_2.items():
             if v[1] in range(0,6):
                 v[1] = v[1] + 7
-        if v[1] in range(6,13):
-            v[1] = v[1] - 5
+            if v[1] in range(6,13):
+                v[1] = v[1] - 5
+        for i, v in results_bpm_4.items():
+                v[1] = v[1] + 2
+        for i, v in results_bpm_2.items():
+            if v[1] in range(0,6):
+                v[1] = v[1] + 5
+            if v[1] in range(6,13):
+                v[1] = v[1] - 7
+        for i, v in results_bpm_4.items():
+                v[1] = v[1] - 2
 
 # Checks in database for key matches
     def key_match_same(key):
@@ -100,11 +109,17 @@ def hackmix():
     key_match_weak(key)
 
 hackmix()
-# print 'Tracks within the BPM range', results_bpm
+
 print 'bpm2',results_bpm_2
+# print 'Tracks within the BPM range', results_bpm
 print 'Tracks in same key within that BPM range', same_match
 print 'Tracks in a strong match key within that BPM range', strong_match
 print 'Tracks in a weak match key within that BPM range', weak_match
+
+
+# stuff to add, known bugs: 
+# being able to match tracks that are at, say 81 bpm, with tracks at 159 bpm 
+# calculations not taking into account whether the vinyl plays at 33 or 45 rpm
 
 
 
